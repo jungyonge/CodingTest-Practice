1cdbda91813cc6bea9a1a290711fd04cb4e00854
jungyonge <jungyong_e@naver.com>
2021-08-02 17:10:19 +0900

    코테 준비

---
+++ src/main/java/com/coding/test/programmers/Ex_29.java
@@ -0,0 +1,37 @@
+package com.coding.test.programmers;
+
+import java.util.Locale;
+
+public class Ex_29 {
+    public static String solution(String new_id) {
+        String answer = "";
+        String temp = "";
+        answer = new_id.toLowerCase(Locale.ROOT);
+        answer = answer.replaceAll("[^0-9a-zA-Z\\\\s._-]","");
+        answer = answer.replaceAll("[.]+",".");
+        answer = answer.replaceAll("^[.]|[.]$","");
+        if(answer.length() == 0){
+            answer = "a";
+        }
+        if(answer.length() > 15){
+            answer = answer.substring(0,15);
+            answer = answer.replaceAll("[.]$","");
+        }
+        while (true){
+            temp = answer.substring(answer.length() -1 );
+            if(answer.length() <= 2){
+                answer = answer + temp;
+            }
+            if(answer.length() > 2){
+                break;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String new_id = "abcdefghijklmn.p";
+        solution(new_id);
+    }
+}
