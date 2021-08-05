1e20ed944445ec6a38bbf278fc8355fad0248299
jungyonge <jungyong_e@naver.com>
2021-08-05 23:53:15 +0900

    코테 준비

---
+++ src/main/java/com/coding/test/programmers/Ex_33.java
@@ -0,0 +1,33 @@
+package com.coding.test.programmers;
+
+public class Ex_33 {
+
+
+    public static int solution(int left, int right) {
+        int answer = 0;
+        int cnt = 0;
+        for(int n = left ;  n <= right ; n ++){
+            cnt = 0;
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    cnt++;
+                }
+            }
+            if(cnt % 2 == 1){
+                answer += n * -1;
+            }else {
+                answer += n;
+            }
+        }
+        System.out.println(answer);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int l = 13;
+        int r = 17;
+        solution(l,r) ;
+    }
+
+}
