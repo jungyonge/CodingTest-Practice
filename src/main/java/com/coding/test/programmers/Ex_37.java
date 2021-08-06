662b634b04b407af61562174267fc3fd0fca481d
jungyonge <jungyong_e@naver.com>
2021-08-06 16:26:57 +0900

    코테 준비

---
+++ src/main/java/com/coding/test/programmers/Ex_37.java
@@ -0,0 +1,49 @@
+package com.coding.test.programmers;
+
+public class Ex_37 {
+    public static int solution(String s) {
+        int answer = s.length();
+        StringBuffer sb = new StringBuffer();
+        int cnt = 1;
+        for(int i = 1 ; i <= s.length()/2   ; i ++){
+            String startWord = s.substring(0, i );
+            System.out.println(startWord);
+            for(int j = 1 ; j < s.length() ; j ++) {
+                if(i * j + i > s.length()){
+                    sb.append(s.substring(i*j));
+                    break;
+                }
+                String compareWord = s.substring(i * j,i * j + i);
+                if(startWord.equals(compareWord)){
+                    cnt++;
+                }else {
+                    if(cnt == 1){
+                        sb.append(startWord);
+                    }else {
+                        sb.append(cnt);
+                        sb.append(startWord);
+                    }
+                    startWord = s.substring(i * j,i * j + i);
+                    cnt = 1;
+                }
+            }
+            if(cnt == 1){
+                sb.append(startWord);
+            }else {
+                sb.append(cnt);
+                sb.append(startWord);
+            }
+            if(answer > sb.toString().length()){
+                answer = sb.toString().length();
+            }
+            cnt = 1;
+            sb = new StringBuffer();
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s= "xababcdcdababcdcd";
+        solution(s);
+    }
+}
