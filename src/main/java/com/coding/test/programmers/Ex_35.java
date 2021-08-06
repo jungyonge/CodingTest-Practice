db4c2d7c9df0bcb463d25e97da02815e62530a40
jungyonge <jungyong_e@naver.com>
2021-08-06 14:59:10 +0900

    코테 준비

---
+++ src/main/java/com/coding/test/programmers/Ex_35.java
@@ -0,0 +1,30 @@
+package com.coding.test.programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Ex_35 {
+    public static int[] solution(int[] numbers) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 0 ; i < numbers.length; i ++){
+            for(int j = 0 ; j < numbers.length ; j++){
+                if(i != j){
+                    int number = numbers[i] + numbers[j];
+                    if(!arr.contains(number)){
+                        System.out.println(number);
+                        arr.add(number);
+                    }
+                }
+            }
+        }
+        Collections.sort(arr);
+        int[] answer = arr.stream().mapToInt(i -> i).toArray();
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] num = {2,1,3,4,1};
+        solution(num);
+    }
+}
