b6ae7dc4fb37d799c04b25df610f2bb4cdf0fdd3
jungyonge <jungyong_e@naver.com>
2021-08-06 15:31:50 +0900

    코테 준비

---
+++ src/main/java/com/coding/test/programmers/Ex_36.java
@@ -0,0 +1,18 @@
+package com.coding.test.programmers;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class Ex_36 {
+    public String solution(int a, int b) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(2016, a-1, b);
+        Date date = cal.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("E", Locale.ENGLISH);
+
+        String answer = sdf.format(date).toUpperCase();
+        return answer;
+    }
+}
