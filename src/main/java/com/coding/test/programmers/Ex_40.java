1ff2280552f2163dfaa1e226a1b989e0e6688b90
jungyonge <jungyong_e@naver.com>
2021-08-11 23:28:12 +0900

    코테 준비

--- src/main/java/com/coding/test/programmers/Ex_40.java
+++ src/main/java/com/coding/test/programmers/Ex_40.java
@@ -1,21 +1,12 @@
 package com.coding.test.programmers;
 
+import java.math.BigInteger;
+
 public class Ex_40 {
     public static long solution(long w, long h) {
-        long answer = 1;
-        long gdc = GCD(w,h);
-        long newW = w / gdc;
-        long newH = h / gdc;
-        long del = 0;
-        if(newH - newW == 1){
-            del =  newH + newW - 1 ;
-        }else {
-            del =  newH * gdc;
-        }
-        answer = (h * w) - del;
-        return answer;
+        int gcd = BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).intValue();
+        return ((long) w * (long) h) - ((((long) w / gcd) + ((long) h / gcd) - 1) * gcd);
     }
-
     static long GCD(long a, long b){ // 최대공약수
         if (a%b == 0) {
             return b;
@@ -24,8 +15,8 @@ public class Ex_40 {
     }
 
     public static void main(String[] args) {
-        int w = 10;
-        int h = 6;
+        int w = 4;
+        int h = 4;
 
         solution(w,h);
     }
