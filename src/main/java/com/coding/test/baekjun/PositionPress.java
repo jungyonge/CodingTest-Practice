fe6a20469fdb7b32d88f0e4ca8d79615c5ec96a7
jungyonge <jungyong_e@naver.com>
2022-10-29 16:51:24 +0900

    코테

--- src/main/java/com/coding/test/baekjun/PositionPress.java
+++ src/main/java/com/coding/test/baekjun/PositionPress.java
@@ -1,32 +1,88 @@
 package com.coding.test.baekjun;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 public class PositionPress {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int cnt = scanner.nextInt();
+//        ArrayList<Integer> numSet = new ArrayList<>();
+//        Integer[] numArr = new Integer[cnt];
+//        for(int i = 0 ; i < cnt ; i++){
+//            int num = scanner.nextInt();;
+//            if(!numSet.contains(num)){
+//                numSet.add(num);
+//            }
+//            numArr[i] = num;
+//        }
+//        StringBuilder sb = new StringBuilder();
+//
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        numSet.sort(Collections.reverseOrder());
+//
+//        int count = 0;
+//        for (int i = 0; i < numArr.length; i++) {
+//            if (!map.containsKey(numArr[i])) {
+//                map.put(numArr[i], count++);
+//            }
+//        }
+//
+//        for(int i : numArr){
+//            int sum = 0;
+//            if(map.containsKey(i)){
+//                sum = map.get(i);
+//            }else {
+//                int temp = numSet.size() - numSet.indexOf(i) - 1;
+//                map.put(i ,temp);
+//                sum = temp;
+//            }
+//
+//            sb.append(sum).append(" ");
+//        }
+//        System.out.println(sb.toString().substring(0,sb.toString().length() - 1));
+//    }
 
-        int cnt = scanner.nextInt();
-        Set<Integer> numSet = new HashSet<>();
-        int[] numArr = new int[cnt];
-        for(int i = 0 ; i < cnt ; i++){
-            int num = scanner.nextInt();;
-            numSet.add(num);
-            numArr[i] = num;
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = new Integer(br.readLine());
+        int[] arr = new int[N];
+        int[] arrClone;
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
         StringBuilder sb = new StringBuilder();
-        for(int i : numArr){
-            int sum = 0;
-            for(Integer j : numSet){
-                if(i >j){
-                    sum++;
-                }
-            }
-            sb.append(sum).append(" ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(sb.toString().substring(0,sb.toString().length() - 1));
+
+        arrClone = arr.clone();
+
+        Arrays.sort(arrClone);
+
+        for (int i = 0; i < arrClone.length; i++) {
+            if (!map.containsKey(arrClone[i])) {
+                map.put(arrClone[i], count++);
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(map.get(arr[i])).append(" ");
+        }
+
+        System.out.println(sb);
     }
 }
