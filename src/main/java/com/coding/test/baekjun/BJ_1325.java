52b281c2207e37287cba650f9d38dec2c36199a0
jungyonge <jungyong_e@naver.com>
2022-12-15 15:35:50 +0900

    코딩 테스트 연습

---
+++ src/main/java/com/coding/test/baekjun/BJ_1325.java
@@ -0,0 +1,76 @@
+package com.coding.test.baekjun;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BJ_1325 {
+
+    //양방향 간선
+    //그래프 grahp
+    //연결된 요소의 개수
+    static int[][] graph ;
+    static int V;
+    static int E;
+    static boolean[] visited;
+    static int result;
+    static int MAX_COUNT  = Integer.MIN_VALUE;
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        V = sc.nextInt();
+        E = sc.nextInt();
+        graph = new int[V+1][V+1];
+        visited = new boolean[V+1];
+
+        int a,b;
+        for(int i = 0; i < E; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+
+            // 간선 연결
+            graph[b][a] = 1;
+        }
+
+        ArrayList<Integer> count = new ArrayList<>();
+        // dfs 탐색
+        for(int i = 1; i <= V; i++) {
+            visited = new boolean[V+1];
+            result = 0;
+            if(visited[i] == false) { // 방문한 적 없는 노드라면 dfs.
+                dfs(i);
+            }
+            if(result > MAX_COUNT){
+                MAX_COUNT = result;
+                count.clear();
+            }
+            if(result >= MAX_COUNT){
+                count.add(i);
+            }
+        }
+        Collections.sort(count);
+        for(Integer i : count){
+            System.out.println(i);
+        }
+
+        sc.close();
+        return;
+    }
+
+    public static void dfs(int index) {
+        if(visited[index] == true)
+            return;
+        else {
+            visited[index] = true;
+            for(int i = 1; i <= V; i++) {
+                if(graph[index][i] == 1) {
+                    dfs(i);
+                    result++;
+                }
+            }
+        }
+    }
+}
